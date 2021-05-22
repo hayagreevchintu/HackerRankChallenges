5e0a11e8f2b6400a3db1c68002bb828d2d75579a
Hayagreev <47315980+hayagreevchintu@users.noreply.github.com>
2021-05-23 00:12:12 +0530

    Add files via upload

---
+++ companyLogo.py
@@ -0,0 +1,15 @@
+def fun(string):
+    all_freq = {}
+    for i in string:
+        if i in all_freq:
+            all_freq[i] += 1
+        else:
+            all_freq[i] = 1
+    print(all_freq)
+    all_freq_list=sorted(all_freq,key=all_freq.get,reverse=True)
+    print(all_freq_list)
+    for i in range(3):
+        print(all_freq_list[i] + ' '+str(all_freq[all_freq_list[i]]))
+
+string = sorted(list(input()))
+fun(string)
