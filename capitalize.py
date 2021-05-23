631171f6bc2cb189670ecd18fcff8dc1e98046f3
Hayagreev <47315980+hayagreevchintu@users.noreply.github.com>
2021-05-23 17:40:04 +0530

    Update capitalize.py

--- capitalize.py
+++ capitalize.py
@@ -1,12 +1,18 @@
+#Link to the challenge:
+#https://www.hackerrank.com/challenges/capitalize/problem
+
+#Stub code:
 import math
 import os
 import random
 import re
 import sys
 
+#Solution begins here:
 def capitalize(s):
     return ' '.join(list(map(str.capitalize, s.split(' '))))
 
+#Stub code:
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
     s = input()
