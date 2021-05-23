65e1fd719bc6c12faf546fc96b0fa824e44f1338
Hayagreev <47315980+hayagreevchintu@users.noreply.github.com>
2021-05-23 17:41:56 +0530

    Update leftRotation.py

--- leftRotation.py
+++ leftRotation.py
@@ -1,16 +1,21 @@
+#Link to the challenge:
+#https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem
+
+#Stub code:
 import math
 import os
 import random
 import re
 import sys
 
+#Solution begins here:
 def rotLeft(a, d):
-    # Write your code here
     for i in range(d):
         temp = a.pop(0)
         a.append(temp)
     return a
 
+#Stub code:
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
     first_multiple_input = input().rstrip().split()
