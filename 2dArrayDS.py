0a6ca439099d2a901dbb6f76239b62063b89d979
Hayagreev <hayagreev.chintu1@gmail.com>
2021-06-22 13:10:29 +0530

    solved 2d array-ds

--- 2dArrayDS.py
+++ 2dArrayDS.py
@@ -21,14 +21,9 @@ def hourglassSum(arr):
 #Stub code:
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     arr = []
-
-    for _ in xrange(6):
-        arr.append(map(int, raw_input().rstrip().split()))
-
+    for _ in range(6):
+        arr.append(map(int, input().rstrip().split()))
     result = hourglassSum(arr)
-
     fptr.write(str(result) + '\n')
-
     fptr.close()
