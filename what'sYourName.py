0bd55432680e87e562b35a68f3649e48b5072e88
Hayagreev <hayagreev.chintu1@gmail.com>
2021-07-05 16:04:52 +0530

    solved what's your name

---
+++ what'sYourName.py
@@ -0,0 +1,12 @@
+#Link to the challenge:
+#https://www.hackerrank.com/challenges/whats-your-name/problem
+
+#Solution begins here:
+def print_full_name(first, last):
+    print("Hello {} {}! You just delved into python.".format(first, last))
+
+#Stub code:
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
