547cf10f6a0c0706455694f71f7c285061191f9a
Hayagreev <hayagreev.chintu1@gmail.com>
2021-07-12 11:51:37 +0530

    solved arrays-ds

--- arrays-ds.py
+++ arrays-ds.py
@@ -21,14 +21,9 @@ def reverseArray(a):
 #Stub code:
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     arr_count = int(input().strip())
-
     arr = list(map(int, input().rstrip().split()))
-
     res = reverseArray(arr)
-
     fptr.write(' '.join(map(str, res)))
     fptr.write('\n')
-
     fptr.close()
